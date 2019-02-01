fd5d2c6d21090404012355072ee29d5d94bfcb7d
42b883 <42b883@gmail.com>
2019-02-01 12:12:34 +0600

    Routes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
     <div>
       <header style={headerStyle}>
           <h1>Todo List</h1>
+          <Link style={LinkStyle} to="/">Home</Link> | <Link style={LinkStyle}
+          to="/about">About</Link>
       </header>
     </div>
   )
+}
+
+const LinkStyle = {
+    color: '#fff',
+    textDecoration: 'none',
+    fontFamily: 'Arial',
+    fontWeight: 'lighter',
+    fontSize: 14,
+    paddingBottom: '6px',
+    display: 'inline-block'
 }
 
 const headerStyle = {
